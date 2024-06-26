3cf86c10f1ebd02e8399eb0a52350c7dcf59119d
instant-rice <canetaei@gmail.com>
2024-06-27 04:02:24 +0900

    update-ver

---
+++ Spring_project/src/main/java/com/ohgiraffers/spring_project/PageController.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.spring_project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/leePage")
+    public String leePageRedirect() {
+        return "page/Lee/Lee";
+    }
+
+    @GetMapping("/seungYeopPage")
+    public String seungYeopPageRedirect() {
+        return "page/seungYeop/SeungYeop";
+    }
+
+    @GetMapping("/Elemental")
+    public String ElementalRedirect(){
+        return "page/seungYeop/Elemental";
+    }
+
+    @GetMapping("/minJooPage")
+    public String minJooPageRedirect(){return "page/MinJoo/MinJoo";}
+
+    @GetMapping("/MainDayoung")
+    public String MainDayoungRedirect(){
+        return "page/Dayoung/Dayoung";
+    }
+}
